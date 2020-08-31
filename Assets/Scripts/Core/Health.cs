11551c7f54881765e8784a3a90f042c5355a37bc
Nathan Wiles <ntwiles@gmail.com>
2020-08-31 18:53:13 -0400

    Created Health script.

---
+++ Assets/Scripts/Core/Health.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ProjectRogue.Core
+{
+    public class Health : MonoBehaviour
+    {
+        // Settings
+        [SerializeField] private int startingHealth = 100;
+
+        // State
+        [SerializeField] private int currentHealth;
+
+        // Events 
+        [SerializeField] private UnityEvent DeathEvent;
+        
+        void Awake()
+        {
+            currentHealth = startingHealth;
+        }
+
+        public void Damage(int damageAmount)
+        {
+            currentHealth -= damageAmount;
+
+            if (currentHealth <= 0)
+            {
+                DeathEvent.Invoke();
+            }
+        }
+    }
+}

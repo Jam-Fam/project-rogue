d6604177f8cd3cf4f113eb037736e1486477a1a2
Nathan Wiles <ntwiles@gmail.com>
2020-09-01 20:37:53 -0400

    Added ability to drop down to below floors.

--- Assets/Scripts/Maps/MapGenerator.cs
+++ Assets/Scripts/Maps/MapGenerator.cs
@@ -1,7 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
-
+using System.Xml;
 using UnityEngine;
 
 namespace ProjectRogue.Maps
@@ -13,7 +13,7 @@ namespace ProjectRogue.Maps
 
         // State
         private RoomData[,] rooms;
-        private List<RoomTemplate> roomTemplatesNoHole, roomTemplatesBottomHole;
+        private List<RoomTemplate> roomTemplatesClosedBottom, roomTemplatesOpenBottom;
 
         public RoomData[,] GenerateMap(MapGenerationSettings mapGenSettings)
         {
@@ -24,8 +24,6 @@ namespace ProjectRogue.Maps
             rooms = rooms = new RoomData[mapGenSettings.NumRoomsX, mapGenSettings.NumRoomsY];
 
             generateRooms();
-            //generateMainPath();
-            //generateSidePaths();
 
             Resources.UnloadUnusedAssets();
 
@@ -34,8 +32,8 @@ namespace ProjectRogue.Maps
 
         private void loadRoomTemplates()
         {
-            roomTemplatesNoHole = RoomTemplateLoader.LoadRoomsSolidBottom(mapGenSettings);
-            roomTemplatesBottomHole = RoomTemplateLoader.LoadRoomsOpenBottom(mapGenSettings);
+            roomTemplatesClosedBottom = RoomTemplateLoader.LoadRoomsSolidBottom(mapGenSettings);
+            roomTemplatesOpenBottom = RoomTemplateLoader.LoadRoomsOpenBottom(mapGenSettings);
         }
 
         private void generateRooms()
@@ -44,95 +42,17 @@ namespace ProjectRogue.Maps
             {
                 for (int iy = 0; iy < mapGenSettings.NumRoomsY; iy++)
                 {
-                    rooms[ix, iy] = generateRoom(new Vector2Int(ix, iy), roomTemplatesNoHole);
+                    rooms[ix, iy] = generateRoom(new Vector2Int(ix, iy), roomTemplatesClosedBottom);
                 }
             }
-        }
 
-        /*
-        private void generateMainPath()
-        {
-            Vector2Int currentPosition = new Vector2Int(0, Random.Range(0, numRoomsY));
-            List<RoomTemplateData> templates = templatesNoHole;
-
-            // Generate starting room.
-            rooms[currentPosition.x, currentPosition.y] = generateRoom(currentPosition, templates);
-            rooms[currentPosition.x, currentPosition.y].IsStartingRoom = true;
-
-            int attempts = 0;
-
-            while (true && attempts < 100)
+            // Replace one room on each floor with an opened one.
+            for (int i = 1; i < mapGenSettings.NumRoomsY; i++)
             {
-                // Stop if we've reached the opposite wall.
-                if (currentPosition.x == numRoomsX - 1) break;
-
-                // Create a list of directions in which we might place the next room.
-                List<BranchDirection> branchDirections = new List<BranchDirection>();
-
-                // If we can go right, add that to the list.
-                if (checkCanGenerateRoom(currentPosition + new Vector2Int(1, 0)))
-                {
-                    branchDirections.Add(BranchDirection.Right);
-                }
-
-                // Same if we can go down.
-                if (checkCanGenerateRoom(currentPosition + new Vector2Int(0, -1)))
-                {
-                    branchDirections.Add(BranchDirection.Down);
-                }
-
-                // And if we can go up.
-                if (checkCanGenerateRoom(currentPosition + new Vector2Int(0, 1)))
-                {
-                    branchDirections.Add(BranchDirection.Up);
-                }
-
-                // Now pick a random direction from the list.
-                BranchDirection directionChoice = branchDirections[Random.Range(0, branchDirections.Count)];
-
-                switch (directionChoice)
-                {
-                    case BranchDirection.Right:
-                        {
-                            templates = templatesNoHole;
-                            currentPosition += new Vector2Int(1, 0);
-                            break;
-                        }
-                    case BranchDirection.Down:
-                        {
-                            // We need to make a hole so we can go down.
-                            rooms[currentPosition.x, currentPosition.y] = generateRoom(currentPosition, templatesBottomHole);
-
-                            templates = templatesNoHole;
-                            currentPosition += new Vector2Int(0, -1);
-
-                            break;
-                        }
-                    case BranchDirection.Up:
-                        {
-                            templates = templatesBottomHole;
-                            currentPosition += new Vector2Int(0, 1);
-                            break;
-                        }
-                }
-
-                // Choose a random room from the current template.
-                rooms[currentPosition.x, currentPosition.y] = generateRoom(currentPosition, templates);
-
-                attempts++;
+                int randomRoom = UnityEngine.Random.Range(0, mapGenSettings.NumRoomsX);
+                rooms[randomRoom, i] = generateRoom(new Vector2Int(randomRoom, i), roomTemplatesOpenBottom);
             }
         }
-        private bool checkCanGenerateRoom(Vector2Int position)
-        {
-            bool available = (position.x < numRoomsX)
-                && (position.x >= 0)
-                && (position.y < numRoomsY)
-                && (position.y >= 0)
-                && !rooms[position.x, position.y].Occupied;
-
-            return available;
-        }
-        */
 
         private RoomData generateRoom(Vector2Int position, List<RoomTemplate> templates)
         {
@@ -141,11 +61,6 @@ namespace ProjectRogue.Maps
             RoomData room = new RoomData(position, chosenTemplate);
 
             return room;
-        }
-
-        private enum BranchDirection
-        {
-            Right, Up, Down
         }
     }
 
